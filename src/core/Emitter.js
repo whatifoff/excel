dc35fa95f1f09ee029e6cbdac1743144c766fb94
Nikita <whatifoff@yandex.ru>
2020-07-07 22:33:08 +0300

    Finish redux and table logic

--- src/core/Emitter.js
+++ src/core/Emitter.js
@@ -3,30 +3,47 @@ export class Emitter {
     this.listeners = {}
   }
 
+  // dispatch, fire, trigger
+  // Уведомляем слушателе если они есть
+  // table.emit('table:select', {a: 1})
   emit(event, ...args) {
     if (!Array.isArray(this.listeners[event])) {
       return false
     }
-
-    this.listeners[event].forEach((listener) => {
+    this.listeners[event].forEach(listener => {
       listener(...args)
     })
-
     return true
   }
 
+  // on, listen
+  // Подписываемся на уведомление
+  // Добавляем нового слушателя
+  // formula.subscribe('table:select', () => {})
   subscribe(event, fn) {
     this.listeners[event] = this.listeners[event] || []
     this.listeners[event].push(fn)
-
     return () => {
-      this.listeners[event] = this.listeners[event].filter(
-        (listener) => listener !== fn()
-      )
+      this.listeners[event] =
+        this.listeners[event].filter(listener => listener !== fn)
     }
   }
 }
 
+// Example
 // const emitter = new Emitter()
-// emitter.subscribe('vladilen', (data) => console.log(data))
-// emitter.emit('vladilen', 42)
+//
+// const unsub = emitter.subscribe('vladilen', data => console.log(data))
+// emitter.emit('1231231', 42)
+//
+// setTimeout(() => {
+//   emitter.emit('vladilen', 'After 2 seconds')
+// }, 2000)
+//
+// setTimeout(() => {
+//   unsub()
+// }, 3000)
+//
+// setTimeout(() => {
+//   emitter.emit('vladilen', 'After 4 seconds')
+// }, 4000)
